71a735ae309b6be5a3de205b24f9b7c9f9fb1c2d
Dylan Conley <dylanconley@shaw.ca>
2026-02-18 09:09:54 -0700

    JS rough framework

---
+++ map.js
@@ -0,0 +1,24 @@
+
+// From https://leafletjs.com/examples/quick-start/
+var map = L.map('map').setView([51.04, -114.06], 11);  // Calgary approx city center from online search
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+function startconnection() {
+    
+}
+
+function endConnection() {
+
+}
+
+function settopic() {
+
+}
+
+function publishstatus() {
+    
+}
